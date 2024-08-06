815e46a0ae36b6c044140616a25fde26c023d7e8
along <wuhl6@mail2.sysu.edu.cn>
2024-08-06 16:15:59 +0800

    feat: add script to verify github device

--- cyber-news-crawler/crawler/github/github_device_verification.py
+++ cyber-news-crawler/crawler/github/github_device_verification.py
@@ -7,16 +7,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+# 1 hour
+LOAD_TIMEOUT = 3600
+
 
 def verify_device():
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.set_page_load_timeout(600)
+    driver.set_page_load_timeout(LOAD_TIMEOUT)
 
     try:
         driver.get("https://github.com/login")
-        input_element = WebDriverWait(driver, 600).until(
+        input_element = WebDriverWait(driver, LOAD_TIMEOUT).until(
             EC.visibility_of_element_located((By.ID, "otp"))
         )
         logger.info("input element located")
@@ -24,15 +27,15 @@ def verify_device():
         input_element.send_keys(verification_code)
         button = driver.find_element(By.CLASS_NAME, "btn-primary btn btn-block")
         button.send_keys(Keys.ENTER)
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, LOAD_TIMEOUT).until(
             EC.presence_of_element_located(
                 (By.CLASS_NAME, "AppHeader-context-item-label")
             )
         )
-        logger.info("label element located")
+        fs.save(driver.page_source, "verified.html")
         logger.info("current_url:", driver.current_url)
         logger.info("cookies:", driver.get_cookies())
-        fs.save(driver.page_source, "verified.html")
+        logger.info("verify device ok")
     except Exception as e:
         logger.error(f"verify device throws: {e}")
     finally:
